929525991273e9254957621b3aea55c9facc36b0
Qsalman <qosaisalman@gmail.com>
2020-12-16 13:42:23 +0200

    Lab06

---
+++ scrip.js
@@ -0,0 +1,58 @@
+
+// topic one 
+alert("Hello, My Name is Qusai")
+
+
+// define a variable
+var number = 15;          // variable type number
+var drink  = "DRINKS";    // variable type strin  
+var correct = true;       // variable boolean that is true 
+var notcorrect =  false;  // variable boolean that is false
+
+// alert(number);
+//  alert(drink);
+//  alert(correct);
+// alert(notcorrect);
+
+
+// topic tow
+
+// if number is larger than 10
+//  if 15 is larger thhan 10
+// if 5 is larger than 10
+if (number > 10 ){
+ alert(number);
+
+} else {
+  alert("the number is less that 10")
+}
+
+
+ if(number <10) {
+
+alert(number);
+} else{
+
+alert("the number is larger than 10")
+}
+
+if(drink == "Drinks"){
+alert("yes it is ")}
+else{
+
+ alert("no its not")
+}
+
+
+//if("notcorrect"){
+
+//   alert("correct")
+// }
+
+
+// if the number is equal or larger than 10
+//if(number =>10){
+
+//alert(number)
+
+//}
